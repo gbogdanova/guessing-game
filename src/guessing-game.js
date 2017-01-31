1ba78253dec41e50e71b5a5fb28c5c6e6fa4f874
max <gaaalo44ka@gmail.com>
2017-01-31 11:06:10 +0300

    GUESSING_GAME: implemented GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,36 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = null;
+    	this.max = null;
+    	this.result = null;
+    }
 
     setRange(min, max) {
+    	if(min > max) {
+    		alert('nevernii diapazon');
+    		return undefined;
+    	}
 
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	if(this.max - this.min <= 1) {
+    		return this.min;
+    	}
+    	else {
+    		this.result = Math.ceil((this.max + this.min) / 2);
+    	    return this.result;
+    	}
     }
 
     lower() {
-
+    	this.max = this.result;
     }
 
     greater() {
-
+    	this.min = this.result;
     }
 }
 
